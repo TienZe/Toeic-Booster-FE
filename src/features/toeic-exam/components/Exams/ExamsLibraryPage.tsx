5c2f321604b48fd211769ffdf01c590042c36197
TienZe <hertyv72@gmail.com>
2025-06-01 23:02:55 +0700

    Search and filter at toeic browser page

--- src/features/toeic-exam/components/Exams/ExamsLibraryPage.tsx
+++ src/features/toeic-exam/components/Exams/ExamsLibraryPage.tsx
@@ -7,30 +7,41 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-import { useCallback, useEffect, useState } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useCallback, useMemo, useState } from "react";
 import DotLoadingProgress from "../../../../components/UI/DotLoadingProgress";
 import ExamCard from "../../../home/components/ExamCard";
-import { useSearchParams } from "react-router-dom";
 import SearchIcon from "@mui/icons-material/Search";
-import { debounce } from "lodash";
 import usePaginatedToeicExams from "../../../../hooks/usePaginatedToeicExams";
 import UserToeicInfoLayout from "../../../../components/layout/toeic/UserToeicInfoLayout";
+import useDebounce from "../../../../hooks/useDebounce";
+import useToeicCategories from "../../../../hooks/useToeicCategories";
+import { Clear } from "@mui/icons-material";
 
-const LIMIT = 9;
+const EXAM_PAGE_SIZE = 9;
+
+interface IFilterExamForm {
+  search: string;
+  filterCategory?: number;
+}
+
+const DEFAULT_EXAM_FILTER_FORM: IFilterExamForm = {
+  search: "",
+  filterCategory: undefined,
+};
 
 const ExamsLibraryPage = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const tagId = searchParams.get("tag_id") || "";
   const [page, setPage] = useState(0);
-  const [selectedTag, setSelectedTag] = useState<Tag | null>();
-  const [search, setSearch] = useState("");
-  const [searchValueDebounce, setSearchValueDebounce] = useState("");
 
-  useEffect(() => {
-    if (!tagId) {
-      setSelectedTag(null);
-    }
+  const [filterFormData, setFilterFormData] = useState<IFilterExamForm>(
+    DEFAULT_EXAM_FILTER_FORM,
+  );
+
+  const resetPage = useCallback(() => {
+    setPage(0);
+  }, []);
+
+  const debouncedSearch = useDebounce(filterFormData.search, {
+    callbackFn: resetPage,
   });
 
   const { data: paginatedExams, isLoading: isLoadingPaginatedExams } =
@@ -38,113 +49,131 @@ const ExamsLibraryPage = () => {
       {},
       {
         page,
-        limit: LIMIT,
+        limit: EXAM_PAGE_SIZE,
+        search: debouncedSearch,
+        filteredCategory: filterFormData.filterCategory,
       },
     );
 
-  const tags = ["2021", "2022"];
-  const isPendingTags = false;
+  const { data: categories } = useToeicCategories();
 
-  const handleTagClick = (tag: string) => {
-    setSelectedTag(tag);
-    setSearchParams(tag.id !== "" ? { tag_id: tag.id } : {});
-    setPage(1);
+  const categoryItems = useMemo(() => {
+    const items = categories?.map((category) => ({
+      label: category.category,
+      value: category.id,
+    }));
+
+    return [
+      {
+        label: "All",
+        value: undefined,
+      },
+      ...(items || []),
+    ];
+  }, [categories]);
+
+  const handleTagClick = (categoryId: number | undefined) => {
+    setFilterFormData({
+      ...filterFormData,
+      filterCategory: categoryId,
+    });
+
+    setPage(0);
   };
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(event.target.value);
-    setPage(1);
-    debouncedSearch(event.target.value);
+    setFilterFormData({
+      ...filterFormData,
+      search: event.target.value,
+    });
   };
 
-  const debouncedSearch = useCallback(
-    debounce((value: string) => {
-      setSearchValueDebounce(value);
-    }, 1000),
-    [],
-  );
   return (
     <UserToeicInfoLayout>
       <Typography variant="h4" sx={{ marginBottom: 2 }}>
         Exam Library
       </Typography>
-      {isPendingTags || isLoadingPaginatedExams ? (
-        <Box sx={{ marginTop: 2 }}>
-          <DotLoadingProgress />
+
+      <>
+        <Box mb={1}>
+          {categoryItems?.map((category) => {
+            return (
+              <Chip
+                key={category.value}
+                label={category.label}
+                clickable
+                color={
+                  filterFormData.filterCategory === category.value
+                    ? "primary"
+                    : "default"
+                }
+                onClick={() => handleTagClick(category.value)}
+                sx={{ padding: 0.9, marginRight: 0.75 }}
+              />
+            );
+          })}
         </Box>
-      ) : (
-        <>
-          <Box mb={1}>
-            <Chip
-              key={0}
-              label={"All"}
-              clickable
-              color={selectedTag === null ? "primary" : "default"}
-              // onClick={() => handleTagClick({ id: "", name: "All" })}
-              sx={{ padding: 0.9, marginRight: 0.75 }}
-            />
-            {tags?.map((tag) => {
-              return (
-                <Chip
-                  key={"1"}
-                  label={"2021"}
-                  clickable
-                  // color={
-                  //   selectedTag?.name.includes(tag.name) ? "primary" : "default"
-                  // }
-                  onClick={() => handleTagClick(tag)}
-                  sx={{ padding: 0.9, marginRight: 0.75 }}
-                />
-              );
-            })}
+
+        <TextField
+          sx={{
+            width: "100%",
+            paddingRight: 4.5,
+          }}
+          variant="outlined"
+          value={filterFormData.search}
+          onChange={handleSearchChange}
+          size="small"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+            endAdornment: (
+              <InputAdornment
+                position="end"
+                sx={{ display: filterFormData.search ? "block" : "none" }}
+                onClick={() =>
+                  setFilterFormData((prev) => ({
+                    ...prev,
+                    search: "",
+                  }))
+                }
+              >
+                <Clear />
+              </InputAdornment>
+            ),
+          }}
+        />
+
+        {isLoadingPaginatedExams ? (
+          <Box sx={{ marginTop: 2 }}>
+            <DotLoadingProgress />
           </Box>
+        ) : (
+          <Grid2 container rowGap={1.5} my={3}>
+            {paginatedExams && paginatedExams.items.length > 0
+              ? paginatedExams.items.map((exam) => {
+                  return (
+                    <Grid2 key={exam.id} sx={{ width: "275px" }}>
+                      <ExamCard
+                        id={exam.id}
+                        title={exam.name}
+                        duration={"120p"}
+                        totalParticipants={100}
+                        totalComments={123}
+                        numOfParts={7}
+                        numOfQuestions={200}
+                        tags={["Listening", "Reading"]}
+                      />
+                    </Grid2>
+                  );
+                })
+              : "No data"}
+          </Grid2>
+        )}
+      </>
 
-          <TextField
-            sx={{
-              width: "100%",
-              paddingRight: 4.5,
-            }}
-            variant="outlined"
-            value={search}
-            onChange={handleSearchChange}
-            size="small"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <SearchIcon />
-                </InputAdornment>
-              ),
-            }}
-          />
-
-          {isLoadingPaginatedExams ? (
-            <Box sx={{ marginTop: 2 }}>
-              <DotLoadingProgress />
-            </Box>
-          ) : (
-            <Grid2 container rowGap={1.5} my={3}>
-              {paginatedExams && paginatedExams.items.length > 0
-                ? paginatedExams.items.map((exam) => {
-                    return (
-                      <Grid2 key={exam.id} sx={{ width: "275px" }}>
-                        <ExamCard
-                          id={exam.id}
-                          title={exam.name}
-                          duration={"120p"}
-                          totalParticipants={100}
-                          totalComments={123}
-                          numOfParts={7}
-                          numOfQuestions={200}
-                          tags={["Listening", "Reading"]}
-                        />
-                      </Grid2>
-                    );
-                  })
-                : "No data"}
-            </Grid2>
-          )}
-        </>
-      )}
       <Box
         sx={{
           display: "flex",
